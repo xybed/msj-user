4cf840b0cbe7c42d61a7c5417bd21975991e3a04
liqi <qli@qeelyn.hk>
2019-05-10 23:10:38 +0800

    加注解

--- src/main/java/com/msj/user/UserApplication.java
+++ src/main/java/com/msj/user/UserApplication.java
@@ -2,10 +2,12 @@ package com.msj.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @Configuration
+@EnableDiscoveryClient
 public class UserApplication {
 
     public static void main(String[] args) {
